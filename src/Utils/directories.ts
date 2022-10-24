9be9c024ca9ed7d332ae8cc4b97bcba456be3f7f
cristian021195 <cristiangramajo015@gmail.com>
2022-10-23 21:57:03 -0300

    copiamos, (en realidad movimos para probar) la carpeta data dentro de la carpeta publica y ver si funciona la peticion  de la api para con los archivos .json de /data

--- src/Utils/directories.ts
+++ src/Utils/directories.ts
@@ -1,4 +1,4 @@
-export const DIRECTORIES = {
+/*export const DIRECTORIES = {
     TEMPLATE_DIR: "\\docs\\templates\\",
     EXCEL_DIR: "\\data\\excel\\",
     JSON_DIR: "\\data\\json\\",
@@ -16,5 +16,25 @@ export const DATA_DIRECTORIES_ARR = [
     "\\data\\excel\\precios\\",
     "\\data\\json\\horarios\\",
     "\\data\\json\\precios\\",
-]// cwd() --> D:\\PRACTICAS WEB\\PROYECTOS\\horabondi
+]*/
+export const DIRECTORIES = {
+    TEMPLATE_DIR: "\\public\\docs\\templates\\",
+    EXCEL_DIR: "\\public\\data\\excel\\",
+    JSON_DIR: "\\public\\data\\json\\",
+    EXCEL_DIR_HORARIOS: "\\public\\data\\excel\\horarios\\",
+    EXCEL_DIR_PRECIOS: "\\public\\data\\excel\\precios\\",
+    JSON_DIR_HORARIOS: "\\public\\data\\json\\horarios\\",
+    JSON_DIR_PRECIOS: "\\public\\data\\json\\precios\\",
+    DATA_DIR: "\\public\\data\\",
+}
+
+export const DATA_DIRECTORIES_ARR = [
+    "\\public\\data\\excel\\",
+    "\\public\\data\\json\\",
+    "\\public\\data\\excel\\horarios\\",
+    "\\public\\data\\excel\\precios\\",
+    "\\public\\data\\json\\horarios\\",
+    "\\public\\data\\json\\precios\\",
+]
+// cwd() --> D:\\PRACTICAS WEB\\PROYECTOS\\horabondi
 // JSON_DIR_HORARIOS+'exprebus-38-sabados-ns.json'
