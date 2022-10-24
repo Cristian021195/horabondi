4d1d9b8e9ee94234e869db4da03352a6188c1872
cristian021195 <cristiangramajo015@gmail.com>
2022-10-23 21:30:18 -0300

    tercer commit, estamos probando el fs de la api para encontrar nuestra carpeta con los json correspondientes

--- pages/api/file-check.ts
+++ pages/api/file-check.ts
@@ -1,10 +1,10 @@
 import { DIRECTORIES } from '../../src/Utils/directories';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 
 
 export default function handler(req: NextApiRequest,  res: NextApiResponse<any>){
+    let exist = existsSync(process.cwd()+DIRECTORIES.JSON_DIR_PRECIOS)
 
-
-  res.status(200).json({ name: 'John Doe'+process.cwd() })
+  res.status(200).json({ name: 'John Doe'+process.cwd(), new_name: 'John Doe'+__dirname, test_bool:  exist})
 }
