05f026c7bb7bebc37bf8fba5feca194f2303e7c6
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2023-01-08 19:57:57 -0300

    Cambio del timeout
    Estaba en 5 segundos, es mucho, esto realentizaba mucho el start de la PWA dejando Splah screen unicamente

--- src/Api/login.ts
+++ src/Api/login.ts
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 export const LOGIN_DEFAULT = axios.create({
     baseURL:'/api/login/default',
-    timeout: 5000
+    timeout: 800
 })
 
 export const CHECK_LOGIN = axios.create({
     baseURL:'/api/session/check',
-    timeout: 5000
+    timeout: 800
 })
